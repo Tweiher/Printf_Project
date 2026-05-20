1dc674701dd1cf98754341d88e1a483cc8a29712
Ty Weiher <ty.weiher@atlasstudents.com>
2026-05-20 19:50:14 +0000

    finished variadic function formula

--- printf_project/custom_printf.c
+++ printf_project/custom_printf.c
@@ -4,13 +4,34 @@
 int customPrintf(const char *formatString, ...){
     va_list args;
     int count = 0;
-
+    int i = 0;
     if (formatString == NULL) {
-        return printString("Error: String is Null");
+        return printString("Error: formatString is Null");
     }
 
     va_start(args,formatString);
 
+    while (formatString[i] != '\0'){
+        if (formatString[i] == '%'){
+            i++;
+            if (formatString[i]=='\0'){
+                count += printChar('%');
+                break;
+            }
+            if (formatString[i] == 'c' ||
+                formatString[i] == 's' ||
+                formatString[i] == 'd' ||
+                formatString[i] == 'i' ||
+                formatString[i] == 'b' ||
+                formatString[i] == '%'){
+            count += handleSpecifier(formatString[i], args);
+                }
+        }
+        else{
+            count += printChar(formatString[i]);
+        }
+        i++;
+    }
     va_end(args);
 
     return count;
