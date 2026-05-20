c33952756c782aa46f6dcbc4126aab3c669dff34
bsern15 <brandon.serna7@gmail.com>
2026-05-20 15:12:34 -0500

    Revert "Format helpers commit"

--- printf_project/custom_printf.h
+++ printf_project/custom_printf.h
@@ -1,12 +1,6 @@
 #ifndef CUSTOM_PRINTF_H
 #define CUSTOM_PRINTF_H
 
-int customPrintf(const char *formatString,...);
-
-int printChar(char c);
-int printString(const char *fortmatString,...);
-char *numberToString(const char *str);
-char *numberToBinary(unsigned int num);
-
+int customPrintf(const char *formatString, ...);
 
 #endif
